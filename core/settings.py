7390cfaf12f3f1ce426843d1aecd2a5ece4ec4b2
Jair <l21281153@toluca.tecnm.mx>
2025-04-24 14:21:24 -0600

    Primer despliegue

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-_ja6man4v=nbi2q6d=+d__cx#hrop!t&b6^e6emo!i))h@=(tw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
