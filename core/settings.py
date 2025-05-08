d4d675e255b49a80d0f59b521d7959dd6e81b0e0
Jair Garduño <jairgarduno47@gmail.com>
2025-05-08 14:53:58 -0600

    Static root

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,12 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
+# Si usas un almacenamiento estático de producción, configura STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
