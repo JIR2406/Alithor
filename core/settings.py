667cc76d17d02914a448696c94e61dacb53373ce
Jair Garduño <jairgarduno47@gmail.com>
2025-05-22 15:08:09 -0600

    Correccion

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')z
+DEBUG = os.environ.get('DEBUG')
 
 if not DEBUG:
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
